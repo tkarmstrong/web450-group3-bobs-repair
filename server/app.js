aa073f1b58e09e43363098288025a7a146abef2d
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-10-31 00:32:11 -0400

    Deploying to production.

--- server/app.js
+++ server/app.js
@@ -42,19 +42,19 @@ const serverPort = process.env.PORT || 3000;
 
 // ! Connect to Development DB - Must comment out for prod build
 
-const mongoDB = 'mongodb+srv://super-admin:Pa$$word1@bcrs1-r0sf0.mongodb.net/BCRS-dev?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, {
-  useNewUrlParser: true,
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connected error: '));
-db.once('open', () => {
-  console.log('Application connected to Atlas MongoDB instance');
-});
+// const mongoDB = 'mongodb+srv://super-admin:Pa$$word1@bcrs1-r0sf0.mongodb.net/BCRS-dev?retryWrites=true&w=majority';
+// mongoose.connect(mongoDB, {
+//   useNewUrlParser: true,
+// });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connected error: '));
+// db.once('open', () => {
+//   console.log('Application connected to Atlas MongoDB instance');
+// });
 
 
 // * Connect to Production DB - Remove comment block then comment dev above before build
-/*
+
 const mongoDB = "MongoDB:mongodb+srv://super-admin:Pa$$word1@bcrs-prod1-zhpta.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, {
   useNewUrlParser: true
@@ -64,7 +64,7 @@ db.on("error", console.error.bind(console, "MongoDB connected error: "));
 db.once("open", function() {
   console.log("Application connected to Atlas MongoDB instance");
 });
-*/
+
 
 
 /** *********************** API routes go below this line ******************* */
