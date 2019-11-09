3e5d719bfda1937d0bbbc8500446cca19d9b2823
Lea <leatrueworthy@Leas-iMac.local>
2019-11-08 18:30:17 -0800

    api roles still in the process

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -40,25 +40,57 @@ export class UserDetailsComponent implements OnInit {
     this.userId = this.route.snapshot.paramMap.get('id');
     console.log('userId is ' + this.userId);
 
-    this.http.get('/api/roles').subscribe(res => {
-      this.roles = res;
-      this.roleTitle = res['roleTitle'];
-      console.table(res);
-      console.log("roletitle is  " + this.roleTitle)
-      console.log("role:  " + this.role);
-      console.log("user role:  " + this.user.role);
-      console.log("user roleTitle" + this.user.roleTitle)
-      console.log("roles:  " + this.roles);
+    this.http.get('/api/users/' + this.userId).subscribe(res => {
+      console.log('The userId in the GET request is ' + this.userId);
+      this.user = res;
+      this.userId = res['userId'];
+      this.firstName = res['firstName']
+      this.lastName = res['lastName']
+      this.phoneNumber = res['phoneNumber']
+      this.address = res['address']
+      this.email = res['email']
+      //this.roleTitle = res['roleTitle']
+      this.selected = this.SelectedRoleTitle = res['roleTitle'];
+      console.log(this.SelectedRoleTitle)
+      console.log('This user is ' + JSON.stringify(this.user));
     }, err => {
-      { { error } }
-    },
-      () => {
-      });
-  }
+      console.log(err);
+    }, () => {
+      this.form.controls.firstName.setValue(this.user.firstName);
+      this.form.controls.lastName.setValue(this.user.lastName);
+      this.form.controls.phoneNumber.setValue(this.user.phoneNumber);
+      this.form.controls.address.setValue(this.user.address);
+      this.form.controls.email.setValue(this.user.email);
+      this.form.controls.roles.setValue(this.user.roles);
 
+      this.http.get('/api/roles').subscribe(res => {
+        this.roles = res;
+        this.roleTitle = res['roleTitle'];
+        console.table(res);
+        //console.log("roletitle is  " + this.roleTitle)
+        //console.log("role:  " + this.role);
+        //console.log("user role:  " + this.user.role);
+        //console.log("user roleTitle" + this.user.roleTitle)
+        //console.log("roles:  " + this.roles);
+      }, err => {
+        console.log(error);
+      })
+    });
+  }
+  ngOnInit() {
+
+    this.form = this.fb.group({
+      firstName: [null, Validators.compose([Validators.required])],
+      lastName: [null, Validators.compose([Validators.required])],
+      phoneNumber: [null, Validators.compose([Validators.required])],
+      address: [null, Validators.compose([Validators.required])],
+      email: [null, Validators.compose([Validators.required])],
+      roles: [null, Validators.compose([Validators.required])]
+    });
+  }
   saveUser() {
     console.log("clicked submit button");
-    this.http.put('/api/users/update/' + this.userId, {
+    this.http.put('/api/users/' + this.userId, {
       firstName: this.firstName,
       lastName: this.lastName,
       phoneNumber: this.phoneNumber,
@@ -72,42 +104,7 @@ export class UserDetailsComponent implements OnInit {
 
     })
   }
-  ngOnInit() {
-    this.http.get('/api/users/' + this.userId).subscribe(res => {
-      console.log('The userId in the GET request is ' + this.userId);
-      this.user = res;
-      this.userId = res['userId'];
-      this.firstName = res['firstName']
-      this.lastName = res['lastName']
-      this.phoneNumber = res['phoneNumber']
-      this.address = res['address']
-      this.email = res['email']
-      this.roleTitle = res['roleTitle']
 
-      this.selected = this.SelectedRoleTitle = res['roleTitle'];
-      console.log(this.SelectedRoleTitle)
-      console.log('This user is ' + JSON.stringify(this.user));
-    }, err => {
-      console.log(err);
-    }, () => {
-      this.form.controls['firstName'].setValue(this.user.firstName);
-      this.form.controls['lastName'].setValue(this.user.lastName);
-      this.form.controls['phoneNumber'].setValue(this.user.phoneNumber);
-      this.form.controls['address'].setValue(this.user.address);
-      this.form.controls['email'].setValue(this.user.email);
-      this.form.controls['roles'].setValue(this.roles)
-    })
-    this.form = this.fb.group({
-      firstName: [null, Validators.compose([Validators.required])],
-      lastName: [null, Validators.compose([Validators.required])],
-      phoneNumber: [null, Validators.compose([Validators.required])],
-      address: [null, Validators.compose([Validators.required])],
-      email: [null, Validators.compose([Validators.required])],
-      roles: [null, Validators.compose([Validators.required])]
-
-
-    })
-  }
   cancel() {
     this.router.navigate(['/session/user-management']);
   }
